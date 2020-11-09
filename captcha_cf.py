c2114387a25782d6328161597b73da81cc7bcc8a
h0nda <41300123+h0nde@users.noreply.github.com>
2020-11-09 01:17:34 +0100

    Update captcha_cf.py

--- captcha_cf.py
+++ captcha_cf.py
@@ -18,6 +18,7 @@ class Cloudflare:
     def __init__(self, url, proxy=None, timeout=10):
         self.url = url.rstrip("/") + "/robots.txt" # prob the lightest page to load
         self.timeout = timeout # used for page, element and redirect timeouts
+        self.active = True
 
         dc = webdriver.DesiredCapabilities.FIREFOX.copy()
         if proxy:
@@ -48,6 +49,17 @@ class Cloudflare:
             WebDriverWait(self._webdriver, self.timeout).until(EC.element_to_be_clickable((By.ID, "cf-hcaptcha-container")))
         elif self.type == CaptchaType.ReCaptchaV2:
             WebDriverWait(self._webdriver, self.timeout).until(EC.element_to_be_clickable((By.ID, "cf-recaptcha-container")))
+            
+    def __enter__(self):
+        return self
+    
+    def __exit__(self, x, y, z):
+        self.close()
+            
+    def close(self):
+        if self.active:
+            self.active = False
+            self._webdriver.quit()
     
     def _captcha_type(self):
         ct = self._webdriver.find_element_by_name("cf_captcha_kind").get_attribute("value")
@@ -73,5 +85,8 @@ class Cloudflare:
                 document.querySelector(".challenge-form").submit()""", captcha_response)
             
         WebDriverWait(self._webdriver, self.timeout).until(EC.url_changes(self._webdriver.current_url))
-        return self._webdriver.execute_script("return navigator.userAgent"), \
-            self._webdriver.get_cookie("cf_clearance")["value"]
+        user_agent = self._webdriver.execute_script("return navigator.userAgent")
+        cf_clearance = self._webdriver.get_cookie("cf_clearance")["value"]
+        
+        self.close()
+        return (user_agent, cf_clearance)
